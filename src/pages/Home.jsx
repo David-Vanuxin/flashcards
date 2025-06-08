3efea7d058baedb2816720d805d7cd0d860aee44
David Vanuhin <asdasd@asd.asd>
2025-06-08 15:37:54 +0300

    На главной теперь показываются только названия модулей Кликнув по названию можно перейти на страницу информации о модуле (такой страницы пока нет)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useGetAllModulesQuery } from "../modules/modulesApi"
 
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 
 import ViewModules from "../modules/ViewModules"
 
@@ -11,7 +16,7 @@ export default function Home() {
 	return (<>
 		<Box>
 			<Typography variant="h5">Все модули</Typography>
-			<ViewModules />
+			<ListModules />
 			<Box sx={{ mt: 2, display: "flex", justifyContent: "center" }}>
 				<Fab 
 					component={Link}
@@ -24,5 +29,46 @@ export default function Home() {
 				</Fab>
 			</Box>
 		</Box>
+	</>)
+}
+
+function ListModules(props) {
+	const { data, error, isLoading } = useGetAllModulesQuery()
+
+	if (error) return (<>
+		<Typography variant="body1">Что-то пошло не так((</Typography>
+	</>)
+
+	if (isLoading) return (<>
+		<Typography variant="body1">Загрузка...</Typography>
+	</>)
+
+	if (data) {
+		if (data.length == 0) return (<>
+			<Typography variant="body1">Чтобы создать модуль нажмите кнопку "+"</Typography>
+		</>)
+
+		return (<>
+			<Table sx={{ border: "none" }} aria-label="collapsible table">
+			<TableBody>
+			{
+				data.map(mod => <ModuleName key={mod.id} mod={mod}/>)
+			}
+			</TableBody>
+			</Table>
+		</>)
+	}
+
+}
+function ModuleName(props) {
+	const navigate = useNavigate()
+	return (<>
+		<TableRow
+			onClick={() => navigate(`/module/${props.mod.id}`)} 
+			sx={{ '& > *': { border: 0 } }}>
+		<TableCell>
+			{props.mod.name}
+		</TableCell>
+		</TableRow>
 	</>)
 }
